27a91e158725f4183d209a00595dd36fedf7ec92
Alena_RA <147716748+Alena-Ryabova@users.noreply.github.com>
2023-11-15 20:43:23 +0500

    homework_1 is done

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,26 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def item_fortest():
+    return Item("Планшет", 15000, 15)
+
+
+def test_item_init(item_fortest):
+    """Когда мы создаем экземпляр класса со значением Х, то get_value вернет нам Х."""
+    assert item_fortest.name == "Планшет"
+    assert item_fortest.price == 15000
+    assert item_fortest.quantity == 15
+
+
+def test_calculate_total_price(item_fortest):
+    assert item_fortest.calculate_total_price() == 225000
+
+
+Item.pay_rate = 0.8
+
+
+def test_apply_discount(item_fortest):
+    assert item_fortest.apply_discount() == 12000
