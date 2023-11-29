d92b409a34114a8bb066591335a0642f6b6fb100
Alena_RA <147716748+Alena-Ryabova@users.noreply.github.com>
2023-11-29 17:40:17 +0500

    homework_4 is done

---
+++ src/phone.py
@@ -0,0 +1,29 @@
+from src.item import Item
+
+
+class Phone(Item):
+
+    # Переопределяем метод базового класса
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim):
+        # Вызываем метод базового класса
+        super().__init__(name, price, quantity)
+        # Дополнительный код
+        self.__number_of_sim = number_of_sim
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    def __add__(self, other):
+        if not isinstance(other, Item):
+            raise ValueError('Складывать можно только объекты Item и дочерние от них.')
+        return self.quantity + other.quantity
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, count_sim):
+        if not isinstance(count_sim, int) or count_sim <= 0:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        self.__number_of_sim = count_sim
